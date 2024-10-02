4d6bf83fc730399b79d7681149d10e1e36d9917d
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2024-10-03 00:57:34 +0530

    database connection

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-    user:"postgres",
-    password:"2003",
-    host:"localhost",
-    database:"perntodo",
-    port:5432
+    user:"todo_database_qy6s_user",
+    password:process.env(DATABASE_PASSWORD),
+    host:"dpg-cruont0gph6c73dm8si0-a",
+    database:"todo_database_qy6s",
+    port:5432,
+    ssl: { rejectUnauthorized: false }
 })
 
 module.exports = pool;
