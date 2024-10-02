3468d45c5a5beb6848dabcf546e777ebda737f40
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2024-10-03 01:51:00 +0530

    defdef

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 const Pool = require("pg").Pool;
-const pool = new Pool({
-    user:"todo_database_qy6s_user",
-    password:process.env.DATABASE_PASSWORD,
-    host:process.env.EXTERNAL_DATABSE_URI,
-    database:"todo_database_qy6s",
-    port:5432,
-})
+// const pool = new Pool({
+//     user:"todo_database_qy6s_user",
+//     password:process.env.DATABASE_PASSWORD,
+//     host:process.env.EXTERNAL_DATABSE_URI,
+//     database:"todo_database_qy6s",
+//     port:5432,
+// })
+const poolConfig = {
+    max:5,
+    min:2,
+    idleTimeoutMillis:600000,
+};
 
-
+poolConfig.connectionString = "postgres://todo_database_qy6s_user:HaXSMVlzIxCkdsBHxOo7AmVAwNErHMQB@dpg-cruont0gph6c73dm8si0-a.oregon-postgres.render.com:5432/todo_database_qy6s"
+const pool = new Pool(poolConfig);
 
 module.exports = pool;
