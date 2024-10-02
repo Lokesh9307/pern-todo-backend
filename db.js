b79f41d2f42276ecbb64ae4beeb303067d7d6319
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2024-10-03 01:00:47 +0530

    error resolve

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
     user:"todo_database_qy6s_user",
-    password:process.env(DATABASE_PASSWORD),
+    password:process.env.DATABASE_PASSWORD,
     host:"dpg-cruont0gph6c73dm8si0-a",
     database:"todo_database_qy6s",
     port:5432,
