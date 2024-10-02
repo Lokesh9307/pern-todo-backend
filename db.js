f946656ae2bc5e08d0e496d463f748a6ab817141
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2024-10-03 01:39:06 +0530

    defefewf

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user:"todo_database_qy6s_user",
     password:process.env.DATABASE_PASSWORD,
-    host:"dpg-cruont0gph6c73dm8si0-a.oregon-postgres.render.com",
+    host:process.env.EXTERNAL_DATABSE_URI,
     database:"todo_database_qy6s",
     port:5432,
 })
