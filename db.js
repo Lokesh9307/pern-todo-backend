2a366e2b1a9d4622fff4f938b07f87f4c4372643
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2024-10-03 16:01:55 +0530

    ffv

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const {Pool} = require("pg");
-require('dotenv').config;
+require('dotenv').config();
 const pool = new Pool({
     user:process.env.USERNAME,
     host:process.env.HOST,
     database:process.env.DATABASE_NAME,
     password:process.env.DATABASE_PASSWORD,
     port:5432,
+    ssl: {
+        rejectUnauthorized: false  // Accept self-signed certificates (optional, depending on your environment)
+    }
 })
 
 
