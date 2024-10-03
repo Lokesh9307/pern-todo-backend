c66e699b282037a7784e7f8e67049eac32fadb4f
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2024-10-03 16:15:02 +0530

    dececec

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const {Pool} = require("pg");
 require('dotenv').config();
 const pool = new Pool({
     user:process.env.USERNAME,
-    host:process.env.HOST,
+    host:"dpg-crv701tds78s73a9m7eg-a",
     database:process.env.DATABASE_NAME,
     password:process.env.DATABASE_PASSWORD,
     port:5432,
