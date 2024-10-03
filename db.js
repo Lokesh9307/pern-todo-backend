5eec63c3fa3a48d6ada5d930eab8a131fb8e4988
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2024-10-03 16:21:13 +0530

    dwdwdwd

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     host:"dpg-crv701tds78s73a9m7eg-a",
     database:process.env.DATABASE_NAME,
     password:process.env.DATABASE_PASSWORD,
-    port:5432,
+    port:5431,
     ssl: {
         rejectUnauthorized: false  // Accept self-signed certificates (optional, depending on your environment)
     }
